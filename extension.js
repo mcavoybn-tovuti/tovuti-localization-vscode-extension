64ec6f26c6d9add8910a1ce5987c5a30182e5ad8
Ben McAvoy <ben.mcavoy@tovutiteam.com>
2021-02-19 22:39:32 -0700

    doing progress on that or stuff maybe

--- extension.js
+++ extension.js
@@ -1,34 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+// External Dependencies
 const vscode = require('vscode');
+
+// Local Dependencies
 const tovutilocalization = require('./tovutilocalization');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-
-var localizationTable = {};
 /**
+ * This function is called when the extension is activated
+ * 
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
 
-	// Initialize a hash table of localizaiton varaibles where the key
-	// is the value of the localization variable
-
-	//@TODO: add a config option for users to specify a custom search directory
+	// @TODO: add a config option for users to specify a custom search directory in case they don't want
+	// to search the current workspace for the language folders.
 	// const configuredDirectory = vscode.workspace.getConfiguration('tovutilocalization.searchDirectory');
 	// console.log(configuredDirectory);
+
+	// Initialize localization hash table based on current workspace folder
 	const currentWorkspace = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders[0].uri.path.slice(1);
 	if (currentWorkspace) {
-		console.log('workspace found: ' + currentWorkspace);
-		 tovutilocalization.initializeLocalizationTable(currentWorkspace + "/language/en-GB/", false, localizationTable)
-		 tovutilocalization.initializeLocalizationTable(currentWorkspace + "/administrator/language/", false, localizationTable);
+		// console.log('Workspace found: ' + currentWorkspace);
+
+		// @TODO make this blocking code. These awaits aren't doing the trick yet
+		// First call to tovutilocalization.findLocalizationVariable fails every time
+		await tovutilocalization.initializeLocalizationTable(currentWorkspace + "/language/en-GB/", false)
+		await tovutilocalization.initializeLocalizationTable(currentWorkspace + "/administrator/language/", false);
 	} else {
-		console.log("tovutilocalizaton failed to activate, root folder not found");
+		console.log("Tovuti Localizaton extension failed to activate, root folder not found");
 		return;
 	}
-	console.log('localization table initialization successful');
+	console.log('Localization table initialization successful');
+	console.log(tovutilocalization.localizationTable)
 
 
 	// The command has been defined in the package.json file
@@ -41,36 +43,64 @@ function activate(context) {
 		let editor = vscode.window.activeTextEditor;
 		if (editor) {
 			// Retrieve the currently highlighed text
-			let cursorPosition = editor.selection.start;
-			let wordRange = editor.document.getWordRangeAtPosition(cursorPosition);
-			let highlight = editor.document.getText(wordRange);
-			vscode.window.showInformationMessage(highlight);
+			let selectionStart = editor.selection.start;
+			let selectionEnd = editor.selection.end;
+			let highlight = editor.document.getText(new vscode.Range(selectionStart, selectionEnd));
+			// console.log('highlighted text: ' + highlight);
 
-			// Check localizationTable for an existing variable
+			// Index into the hash table based on the value of the localization variable
 			let searchResults = tovutilocalization.localizationTable[highlight];
-			if (searchResults.length > 0) {
-				// create a dropdown selection of each variable to be replaced
-				// once the variable is selected, it will replace the current selection
-				// with the appropriate code.
-				console.log('search results: ');
-				console.log(searchResults);
-			}
-			// dropdown should also include an option to add a new variable at
-			// the bottm. @TODO add that here
+			// console.log('search results: ');
+			// console.log(searchResults);
+			// @TODO build out a search function that doesn't require an exact match
 
-			// create event binding for user selection. after a user selects a variable
-			// they will be asked whether they want the code snippet to be enclosed in
-			// <?php ?> brackets or not. We could automate this in the future via static
-			// analysis of the current editor and the file extension of the currently
-			// opened file.
+			if (searchResults.length > 0) {
+				// Populate quickpick options (basically a dropdown) of each variable to be replaced
+				let quickPickItems = searchResults.map(result => {
+					return {
+						label: result.key,
+						detail: result.path
+					};
+				});
+
+				const replaceSelectionWithLocalizationCode = item => {
+					console.log('optionselected');
+					console.log(item);
+
+					// @TODO if selected option is the create new variable
+					// option, then we create a new variable here before
+					// replacing the text
+
+					vscode.window.activeTextEditor.edit(editBuilder => {
+						// @TODO each one of these replaces adds a new change to the clipboard,
+						// we should remove that, as a user I only want to do a single undo command
+						editBuilder.replace(editor.selection, `AxsLanguage::text("${item.label}", "${highlight}")`);
+					});
+				};
+				vscode.window.showQuickPick(quickPickItems, {
+					canPickMany: false,
+					ignoreFocusOut: true,
+					matchOnDescription: true,
+					placeHolder: 'Please select the language variable you want to use',
+					onDidSelectItem: replaceSelectionWithLocalizationCode
+				});
+
+				
+			}
+			// @TODO dropdown should also include an option to add a new variable
+
+			// @TODO add php brackets based on context
 		};
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-function deactivate() {}
+// This method is called when this extension is deactivated.
+// In general, we shouldn't need to use this function.
+function deactivate() {
+	// Do nothing
+}
 
 module.exports = {
 	activate,
--- tovutilocalization.js
+++ tovutilocalization.js
@@ -4,11 +4,8 @@ var fs = require('fs');
 
 let localizationTable = {};
 /**
- * Traverses the given directory and looking for .ini files.
- * If an .ini file is found, it is opened and the localization variables inside
- * are loaded into the localizationTable
  * 
- * So if there is a variable:
+ * If there is a variable:
  * AXS_PAY = "Pay"
  * 
  * defined in the file:
@@ -17,119 +14,129 @@ let localizationTable = {};
  * it will be added to the localizationTable under the 'Pay' key
  * localizationTable["Pay"].push({ key: "AXS_PAY", path: "/languages/en-GB/en-GB.overrides.ini"})
  * 
+ * This is done by traversing the current workspace directory (or a user specified directory)
+ * If an .ini file is found, it is opened and the localization variables inside
+ * are loaded into localizationTable.
  * 
  * @param {string} directory the directory to traverse
  * 
  */
-function initializeLocalizationTable(directory, removeExisting = false) {
-    console.log('starting traversal from directory : ' + directory);
+async function initializeLocalizationTable(directory, removeExisting = false) {
+    // console.log('starting traversal from directory : ' + directory);
+
     if (!fs.existsSync(directory)) {
-        console.log("file not found, exiting: " + directory);
+        console.log("directory not found, exiting: " + directory);
         return;
     }
 
-    var filepathArray = directory.split('.');
-    var fileExtension = filepathArray[filepathArray.length - 1];
+    var fileExtension = directory.split('.')[directory.split('.').length - 1];
 
-    var folderList = fileExtension == 'ini' ? [ directory ] : fs.readdirSync(directory);
-    console.log('folderList:');
-    console.log(folderList);
-    folderList.forEach(fileOrDirectory => {
-        // Complete the filepath for the font file
+    // This check is for debug purposes. Sometimes it's nice to 
+    // reduce the number of localization variables that show up in the debug console
+    // and this piece enables us to pass a single file to the first parameter of this function.
+    var folderList = 
+        fileExtension == 'ini' 
+            ? [ directory ] 
+            : fs.readdirSync(directory);
+    // console.log('folderList:');
+    // console.log(folderList);
+
+    folderList.forEach(async fileOrDirectory => {
+        // If fileOrDirectory is the same as the directory parameter
+        // then we are debugging with a single file, so we don't need
+        // to create a new variable to get the complete filepath.
         var completeFilepath = 
             fileOrDirectory == directory 
                 ? directory 
                 : directory + fileOrDirectory;
-        console.log('completeFilepath: ' + completeFilepath);
-        console.log(fs.lstatSync(completeFilepath).isFile());
+        // console.log('completeFilepath: ' + completeFilepath);
+        // console.log(fs.lstatSync(completeFilepath).isFile());
         // console.log(fileOrDirectory.split('.')[1]);
+
+        // Get the file extension, we only want to open .ini files
+        // and if fileOrDirectory doesn't have an extension, it is a directory.
         var filepathArray = fileOrDirectory.split('.');
         var fileExtension = filepathArray[filepathArray.length - 1];
+        const aFileExistsThere = fs.lstatSync(completeFilepath).isFile();
+        const itsExtensionIsDotIni = fileExtension == 'ini'
+        // console.log('filePathArray:');
         // console.log(filepathArray);
-        console.log(fileExtension);
-        if (
-            // It's a file
-            fs.lstatSync(completeFilepath).isFile()
-            // and it has a file extension
-            // and its file extention is woff
-            && fileExtension == 'ini'
-        ) {
+        // console.log('fileExtension:');
+        // console.log(fileExtension);
+        // console.log('aFileExistsThere: ' + aFileExistsThere);
+        // console.log('itsExtensionIsDotIni: ' + itsExtensionIsDotIni);
 
-            if (removeExisting) {
-                // remove each entry in the localizationTable taken from 
-                // this file
-                Object.keys(localizationTable).forEach(key  => {
-                    localizationTable[key] = localizationTable[key].filter(entry => {
-                        return entry.path != fileOrDirectory;
-                    })
-                })
-            }
+        
+        if (aFileExistsThere && itsExtensionIsDotIni) {
 
             // Open the file 
-            fs.readFile(completeFilepath, 'utf8', (err, data) => {
-                if (err) {
+            // console.log(`attempting to open file at ${completeFilepath}`);
+            await fs.readFile(completeFilepath, 'utf8', (error, data) => {
+
+                if (error) {
                     console.log(`error opening file ${completeFilepath}`);
-                    console.log(`${err}`);
+                    console.log(`${error}`);
                     return;
                 }
-                
-                console.log('file opened successfully');
-                // console.log(data.split("\n"));
-                data.split("\n").forEach(variable => {
-                    // console.log('variable: ' + variable);
-                    if (variable.split('=').length == 2 && variable.slice(0, 1) != ';' ){
-                        const key = variable.split('=')[0].trim();
+                // console.log('file opened successfully.');
+
+                // If we are updating varaibles after a file has been changed
+                if (removeExisting) {
+                    // Remove each entry in localizationTable from this file
+                    Object.keys(localizationTable).forEach(key  => {
+                        localizationTable[key] = localizationTable[key].filter(entry => {
+                            return entry.path != fileOrDirectory;
+                        })
+                    })
+                }
+
+                // Cycle through each line of the file
+                data.split("\n").forEach(line => {
+                    const lineHasVariable = line.split('=').length == 2;
+                    const isNotAComment = line.slice(0, 1) != ';';
+                    if (lineHasVariable && isNotAComment){
+                        // Add the variable to localizationTable
+
+                        // The localization variable key value, e.g. AXS_PAY
+                        const localizationKey = line.split('=')[0].trim();
+
+                        // @TODO get rid of this annoying warning suppression
                         // @ts-ignore
-                        const value = variable.split('=')[1].replaceAll('"', '').trim();
-                        const path = completeFilepath;
+                        // The value of the localization variable with the given key, e.g. "Pay"
+                        const value = line.split('=')[1].replaceAll('"', '').trim();
                         
                         if (!localizationTable[value]) {
                             localizationTable[value] = [];
                         }
-                        localizationTable[value].push({key, path});
+                        const localizationData = {
+                            key: localizationKey,
+                            path: completeFilepath
+                        }
+                        
+                        localizationTable[value].push(localizationData);
                     }
                 })
             });
 
-            // console.log('returning...');
-            // console.log(localizationTable);
-            return;
-
-            // Put a watch on the file so that the localizationTable
-            // so localizationTable is updated if a new variable is added
+            // @TODO Put a watch on the file so the localizationTable
+            // is updated if a variable is added or removed
             // fs.watch(fileOrDirectory, (event, filename) => {
             //     if (filename) {
-            //         console.log(`${CONSOLE_PREFIX}: ${filename} file changed, updating localizationTable`);
-            //         initializeLocalizationTable(fileOrDirectory, true, localizationTable);
+            //         console.log(`${filename} file changed, updating localizationTable`);
+            //         initializeLocalizationTable(directory, true);
             //     }
             // });
             
+            return;
         } else if (filepathArray.length == 1) {
-            // potentialFontFile isn't a file, its a directory
-            // recurse into that directory
-            // let subTable = initializeLocalizationTable(completeFilepath + '/', false, localizationTable);
-            // let combineObjects = (object1, object2) => {
-            //     if (!object2) {
-            //         return object1;
-            //     }
-            //     Object.keys(object2).forEach(key => {
-            //         if (!object1[key]) {
-            //             object1[key] = [];
-            //         }
-            //         object1[key].push(object2[key]);
-            //     });
-            //     return object1;
-            // }
-            // console.log('combining stuff and returning');
-            // console.log(combineObjects(localizationTable, subTable));
-            // return combineObjects(localizationTable, subTable);
-            return initializeLocalizationTable(completeFilepath + '/', false);
+            // Recurse into directory
+            return await initializeLocalizationTable(completeFilepath + '/', false);
         }
     });
 }
 
 function createLocalizationVariable(key, value, filepath) {
-    // write a new localization variable to the file at filepath
+    // Write a new localization variable to the file at filepath
     fs.readFile(filepath, 'utf8', function(err, data) {
         if (err) {
             throw err;
